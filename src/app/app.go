a7de5583e8ca81961044f93e3eb3a333e644dbcb
danial <75265474+daniial79@users.noreply.github.com>
2023-05-12 17:35:34 +0330

    Develop (#1)
    * init[0]: project structure is initiated
    
    * feat[0]: program intro is implemented
    
    * feat[1]: FetchIP and ping executer are implemented
    
    ---------
    
    Co-authored-by: = <=>

---
+++ src/app/app.go
@@ -0,0 +1,8 @@
+package app
+
+import "github.com/daniial79/Connectivity-Test-GoCLI/src/cmd"
+
+func Start() {
+	cmd.LoadProgram()
+	cmd.FetchIp()
+}
---
+++ src/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/daniial79/Connectivity-Test-GoCLI/src/app"
+
+func main() {
+	app.Start()
+}
